790fc029de93174a3189600bfc54d6fdfcd2e9e4
Ramon <ac2262ra0530@pusd.us>
2015-04-14 15:02:02 -0700

    nwqj

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -93,12 +93,21 @@ game.PlayerEntity = me.Entity.extend({
             var xdif = this.pos.x -response.b.pos.x;
             
             console.log("xdif " + xdif + "ydif " + ydif);
-            
-            if(xdif<-35 && this.facing==='right'){
+            if(ydif<-40 && xdif<70 && xdif >-39)
+            {
+                this.body.falling = false;
+                this.body.vel.y = -1;
+            }  else          
+            if(xdif<-35 && this.facing==='right' && (xdif<0)){
                 this.body.vel.x = 0;
                 this.pos.x = this.pos.x -1;
-            }else if(){
-                
+            }else if(xdif<70 &&  this.facing==='left' && xdif>0){
+                this.boody.vel.x = 0;
+                this.pos.x = this.pos.x +1;
+            }
+            if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= 1000){
+                this.lastHit= this.now;
+                response.b.loseHealth(1);
             }
         }
     }
